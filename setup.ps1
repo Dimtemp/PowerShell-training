bf9aebbf9767749c6205e4ba23c546a3604ffb24
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2020-09-06 22:59:50 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,29 +1,42 @@
-# Install-Module Az
-# Connect-AzAccount
-
 function New-AzVMForPowerShellTraining {
 
+    [cmdletbinding()]
     param(
-        $DomainNameLabel = (Read-Host -Title 'Domain Name label')
+        $VMName = 'pwshvm' + (Get-Random -Maximum 1000000),
+        $DomainNameLabel = (Read-Host -Title 'Domain Name label'),
+        $username = (Read-Host -Title 'username'),
+        $Password = 'Pa55w.rd1234'
     )
 
     $vnet = Get-AzVirtualNetwork | Out-GridView -OutputMode single -Title 'Select domain controller vnet'
+    if ($vnet.count -gt 1) { $vnet = $vnet | Out-GridView -OutputMode single -Title 'Select domain controller vnet' }
+
     $subnet = Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet
     if ($subnet.count -NE 1) { throw 'Domain controller vnet should contain only one subnet' }
 
+    $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+    $Cred = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
+
     $AzVMArguments = @{
-        Name = 'VM' + (Get-Random -Maximum 1000000)
-        ResourceGroupName = $vnet.ResourceGorupName
+        Name = $VMName
+        ResourceGroupName = $VMName
         DataDiskSizeInGb = 2
         VirtualNetworkName = $vnet.name
         SubnetName = $subnet.name
-        OpenPorts = 443
-        DomainNameLabel = ''
+        OpenPorts = 3389
+        DomainNameLabel = $VMName   #  ???.eastus.cloudapp.azure.com
+        Credential  = $Cred
+        #AsJob = $true
+        #Location by default same as vnet
+        #Size Standard_DS1_v2 (1 vcpus, 3.5 GiB memory)
+        #Image Win2016Datacenter (default), Win2012R2Datacenter, UbuntuLTS, Debian, ...
+        #Priority = 'Spot' # spot instance
+        #EvictionPolicy = 'Deallocate' # (or 'Delete')
     }
 
-    New-AzVM $AzVMArguments
+    write-verbose @AzVMArguments
+
+    New-AzVM @AzVMArguments
 }
 
-
-New-AzVMForPowerShellTraining -DomainNameLabel 'testdomain.com'
-
+New-AzVMForPowerShellTraining -DomainNameLabel 'powershell.lan'
